5958922fc141cd3c294f83a3c31e6fa85d41c92d
kattisarika <sarika.katti@gmail.com>
2025-08-02 14:52:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from app import get_news_articles
 st.title("NEWS RAG BOT")
 
 # Add a radio button for source selection
-news_source = st.radio("Choose your news source:", ("Times of India", "BBC","ESPN"))
+news_source = st.radio("Choose your news source:", ("Times of India", "BBC","NY Times"))
 
 # Get news based on source
 if news_source == "Times of India":
     articles = get_news_articles(source="TOI")
 elif news_source == "BBC":
     articles = get_news_articles(source="BBC")
-elif news_source == "CNN":
-    articles = get_news_articles(source="CNN")
+elif news_source == "NY Times":
+    articles = get_news_articles(source="NY Times")
 else: 
     articles = []
 
