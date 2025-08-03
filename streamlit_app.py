ee828620c3dc37d13c0779cb4551b0350a168d72
kattisarika <sarika.katti@gmail.com>
2025-08-02 19:30:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,25 @@
 import streamlit as st
 from app import qa_chain
 from app import get_news_articles
+
 st.title("NEWS RAG BOT")
 
-# Add a radio button for source selection
-news_source = st.radio("Choose your news source:", ("Times of India","NY Times","BBC","Weather"))
+option_map = {
+    "Times of India": "TOI",
+    "NY Times": "NY TIMES",
+    "BBC": "BBC",
+    "Weather": "WEATHER"
+}
 
-# Get news based on source
-if news_source == "Times of India":
-    articles = get_news_articles(source="TOI")
+news_source_label = st.radio("Choose your news source:", list(option_map.keys()))
+source_key = option_map[news_source_label]
 
-elif news_source == "NY TIMES":
-    articles = get_news_articles(source="NY Times")
-elif news_source == "BBC":
-    articles = get_news_articles(source="BBC")
-elif news_source == "WEATHER":
-    articles = get_news_articles(source="WEATHER")
-else: 
-    articles = []
+articles = get_news_articles(source=source_key)
 
-# Display articles
-for i, article in enumerate(articles[:10]):
-    st.markdown(f"**{i+1}.** {article}")
+if articles:
+    for i, article in enumerate(articles[:10]):
+        st.markdown(f"**{i+1}.** {article}")
+else:
+    st.write("No articles found for the selected source.")
 
 
